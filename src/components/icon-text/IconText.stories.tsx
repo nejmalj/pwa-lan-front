62949a83a743cfc7f08a5f7f7e48af35a0f01d21
Louise Mendiburu <louisemendiburu@gmail.com>
2025-03-27 16:33:46 +0100

    feat: add IconText component

---
+++ src/components/icon-text/IconText.stories.tsx
@@ -0,0 +1,39 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import IconText from './IconText';
+
+const meta: Meta<typeof IconText> = {
+  component: IconText,
+  tags: ['autodocs'],
+};
+export default meta;
+
+type Story = StoryObj<typeof IconText>;
+
+export const Trophy: Story = {
+    args: {
+        Icon: () => <img src="/trophy.png" />,
+        label: "Convivialité et compétition"
+    },
+};
+
+export const User: Story = {
+    args: {
+        Icon: () => <img src="/user.png" />,
+        label: "MyDigitalSchool"
+    },
+};
+
+export const Clock: Story = {
+    args: {
+        Icon: () => <img src="/clock.png" />,
+        label: "10h00"
+    },
+};
+
+export const Calendar: Story = {
+    args: {
+        Icon: () => <img src="/calendar.png" />,
+        label: "25 mai"
+    },
+};
---
+++ src/components/icon-text/IconText.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './IconText.scss'
+
+export interface IconTextProps {
+    Icon: React.FC;
+    label: string;
+}
+
+function IconText({ Icon, label }: IconTextProps) {
+    return (
+        <div className="iconTextContainer">
+            <Icon />
+            <span>{label}</span>
+        </div>
+    );
+}
+
+export default IconText
