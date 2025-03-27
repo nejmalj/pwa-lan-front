57092513831c7b2b720d33fb1e75fb04f6c014c7
AugustinSeguin <augustinseguin@gmail.com>
2025-03-27 14:59:05 +0100

    add input text component

---
+++ src/components/inputs/InputText.stories.tsx
@@ -0,0 +1,19 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import InputText from './InputText';
+
+const meta = {
+  component: InputText,
+} satisfies Meta<typeof InputText>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    type: "type",
+    value: "value",
+    onChange: () => {}
+  }
+};
---
+++ src/components/inputs/InputText.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './InputText.scss'; 
+
+interface InputTextProps {
+  type: string;
+  value: string;
+  placeholder?: string; 
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const InputText: React.FC<InputTextProps> = ({ type, value, placeholder, onChange }) => {
+  return (
+    <input
+      type={type}
+      value={value}
+      placeholder={placeholder} 
+      onChange={onChange}
+      className="input-text"
+    />
+  );
+};
+
+export default InputText;
