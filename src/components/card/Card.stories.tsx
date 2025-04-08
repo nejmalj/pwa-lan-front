f22c927e3569b57d1420a19e729d4c51750601b9
nejmalj <nejmalejeannic@gmail.com>
2025-04-08 11:11:14 +0200

    Stories for card

---
+++ src/components/card/Card.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Card } from "./Card";
+
+const meta: Meta<typeof Card> = {
+    title: "Components/Card",
+    component: Card,
+    tags: ["autodocs"],
+};
+
+export default meta;
+type Story = StoryObj<typeof Card>;
+
+export const Default: Story = {
+    render: (args) => (
+        <Card {...args}>
+            <h2>Je suis une carte</h2>
+            <p>Avec du contenu à l'intérieur.</p>
+        </Card>
+    ),
+};
