2425e024f3244ae4d7637315bdea4f5c2e996889
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-08 15:17:25 +0200

    fix

--- src/components/buttons/primaryButton/PrimaryButton.stories.tsx
+++ src/components/buttons/primaryButton/PrimaryButton.stories.tsx
@@ -2,42 +2,37 @@ import { Meta, StoryObj } from "@storybook/react";
 import PrimaryButton from "./PrimaryButton.tsx";
 
 const meta: Meta<typeof PrimaryButton> = {
-    title: "Components/PrimaryButton",
-    component: PrimaryButton,
-    argTypes: {
-        variant: {
-            control: { type: "radio" },
-            options: ["primary", "secondary", "danger"],
-        },
-        size: {
-            control: { type: "radio" },
-            options: ["sm", "md", "lg"],
-        },
-        icon: { control: false },
+  title: "Components/PrimaryButton",
+  component: PrimaryButton,
+  argTypes: {
+    variant: {
+      control: { type: "radio" },
+      options: ["primary", "secondary", "danger"]
     },
+    icon: { control: false }
+  }
 };
 
 export default meta;
 type Story = StoryObj<typeof PrimaryButton>;
 
 export const Default: Story = {
-    args: {
-        children: "Bouton",
-        variant: "primary",
-        size: "md",
-    },
+  args: {
+    children: "Bouton",
+    variant: "primary"
+  }
 };
 
 export const Secondary: Story = {
-    args: {
-        ...Default.args,
-        variant: "secondary",
-    },
+  args: {
+    ...Default.args,
+    variant: "secondary"
+  }
 };
 
 export const Danger: Story = {
-    args: {
-        ...Default.args,
-        variant: "danger",
-    },
+  args: {
+    ...Default.args,
+    variant: "danger"
+  }
 };
