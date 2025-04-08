b08ae6ffe631c59caa396ca6de68555ac7dec31e
Louise Mendiburu <louisemendiburu@gmail.com>
2025-04-08 09:49:38 +0200

    Change icons

--- src/components/icon-text/IconText.stories.tsx
+++ src/components/icon-text/IconText.stories.tsx
@@ -12,28 +12,28 @@ type Story = StoryObj<typeof IconText>;
 
 export const Trophy: Story = {
   args: {
-    Icon: () => <img src="/trophy.png" />,
+    Icon: () => <img src="/trophy.svg" />,
     label: "Convivialité et compétition"
   }
 };
 
 export const User: Story = {
   args: {
-    Icon: () => <img src="/user.png" />,
+    Icon: () => <img src="/person.svg" />,
     label: "MyDigitalSchool"
   }
 };
 
 export const Clock: Story = {
   args: {
-    Icon: () => <img src="/clock.png" />,
+    Icon: () => <img src="/clock.svg" />,
     label: "10h00"
   }
 };
 
 export const Calendar: Story = {
   args: {
-    Icon: () => <img src="/calendar.png" />,
+    Icon: () => <img src="/calendar.svg" />,
     label: "25 mai"
   }
 };
