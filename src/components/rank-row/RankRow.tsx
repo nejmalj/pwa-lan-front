c3aee39c059096c64f06b07efe4ab90311f36cb0
Mathis Baveye <mathisbaveye@MacBook-Air-de-Mathis.local>
2025-04-08 11:58:00 +0200

    work in progress

--- src/components/rank-row/RankRow.tsx
+++ src/components/rank-row/RankRow.tsx
@@ -1,32 +1,44 @@
-import IconText from '../icon-text/IconText';
-import './RankRow.scss'
+import IconText from "../icon-text/IconText";
 
 export interface RankRowProps {
-    ranking: number;
-    team: string;
-    win: number;
-    loss: number
+  ranking: number;
+  team: string;
+  win: number;
+  loss: number;
 }
 
-function RankRow({ ranking, team, win, loss }: RankRowProps) {
-    var rankContent:React.ReactElement = <>{ranking}</>
-    if(ranking <= 3){
-        var trophy = "gold"
-        if (ranking == 2){
-            trophy ="silver"
-        }else if(ranking == 3){
-            trophy = "bronze"
-        }
-       rankContent =  IconText({Icon: () => <img src={"/icons/trophy-"+trophy+".svg"} />, label: String(ranking)})
+function RankRow({
+  ranking,
+  team,
+  win,
+  loss
+}: RankRowProps) {
+  var rankContent: React.ReactElement = <>{ranking}</>;
+  if (ranking <= 3) {
+    var trophy = "gold";
+    if (ranking == 2) {
+      trophy = "silver";
+    } else if (ranking == 3) {
+      trophy = "bronze";
     }
-    return (
-        <tr>
-            <td>{rankContent}</td>
-            <td>{team}</td>
-            <td>{String(win)}</td>
-            <td>{String(loss)}</td>
-        </tr>
-    );
+    rankContent = IconText({
+      Icon: () => (
+        <img src={"/icons/trophy-" + trophy + ".svg"} />
+      ),
+      label: String(ranking)
+    });
+  }
+  return (
+    <tr>
+      <td>{rankContent}</td>
+      <td>{team}</td>
+      <td>{String(win)}</td>
+      <td>
+        {String(loss)}
+        <div></div>
+      </td>
+    </tr>
+  );
 }
 
-export default RankRow
+export default RankRow;
--- src/components/ranking/Ranking.tsx
+++ src/components/ranking/Ranking.tsx
@@ -1,34 +1,43 @@
-import './RankRow.scss'
+import RankRow from "../rank-row/RankRow";
+import "./Ranking.scss";
 
 export interface Rank {
-    rank: number;
-    équipe: string;
-    win: number;
-    loss: number;
-  }
-  
-  export interface RankingProps {
-    ranks: Rank[];
-  }
-  
-
-function Ranking({ranks}: RankingProps) {
-
-    return (
-        <table>
-            <thead>
-                <tr>
-                    <td>Place</td>
-                    <td>Équipe</td>
-                    <td>V</td>
-                    <td>D</td>
-                </tr>
-            </thead>
-            <tbody>
-
-            </tbody>
-        </table>
-    );
+  rank: number;
+  équipe: string;
+  win: number;
+  loss: number;
 }
 
-export default Ranking
+export interface RankingProps {
+  ranks: Rank[];
+}
+
+function Ranking({ ranks }: RankingProps) {
+  return (
+    <table className="resultsTable">
+      <thead>
+        <tr>
+          <td>Place</td>
+          <td>Équipe</td>
+          <td>V</td>
+          <td>
+            D <div></div>
+          </td>
+        </tr>
+      </thead>
+      <tbody>
+        {ranks.map((rankItem) => (
+          <RankRow
+            key={rankItem.rank}
+            ranking={rankItem.rank}
+            team={rankItem.équipe}
+            win={rankItem.win}
+            loss={rankItem.loss}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Ranking;
