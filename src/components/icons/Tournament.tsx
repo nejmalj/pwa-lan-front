58fa337cdb332c21eb39006a8cfc0c30359f56e2
Louise Mendiburu <louisemendiburu@gmail.com>
2025-04-08 11:14:35 +0200

    Change icons by components

---
+++ src/components/icons/Tournament.tsx
@@ -0,0 +1,30 @@
+export default function Tournament() {
+  return (
+    <svg
+      width="65"
+      height="32"
+      viewBox="0 0 65 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clip-path="url(#clip0_162_159)">
+        <path
+          d="M42.75 15.56H40.99V9.4H39.23V8.08H32.19V9.4H30.43V6.32H27.79V5H20.75V8.52H27.79V7.2H29.55V12.48H27.79V11.16H20.75V14.68H27.79V13.36H30.43V10.28H32.19V11.6H39.23V10.28H40.11V21.72H39.23V20.4H32.19V21.72H30.43V18.64H27.79V17.32H20.75V20.84H27.79V19.52H29.55V24.8H27.79V23.48H20.75V27H27.79V25.68H30.43V22.6H32.19V23.92H39.23V22.6H40.99V16.44H42.75V15.56ZM26.91 7.64H21.63V5.88H26.91V7.64ZM26.91 13.8H21.63V12.04H26.91V13.8ZM38.35 10.72H33.07V8.96H38.35V10.72ZM26.91 19.96H21.63V18.2H26.91V19.96ZM26.91 26.12H21.63V24.36H26.91V26.12ZM38.35 23.04H33.07V21.28H38.35V23.04Z"
+          fill="white"
+          stroke="white"
+          stroke-width="0.5"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_162_159">
+          <rect
+            width="24"
+            height="24"
+            fill="white"
+            transform="translate(20.75 4)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+}
