4689ae6e5611f05c80ca106bd93a8896a69cb258
ThomasLaforge <thomas.laforge.38@gmail.com>
2025-04-09 14:39:12 +0200

    fix missing props in stories

--- src/components/team/teamCard/TeamCard.stories.tsx
+++ src/components/team/teamCard/TeamCard.stories.tsx
@@ -2,27 +2,47 @@ import type { Meta, StoryObj } from "@storybook/react";
 import TeamCard from "./TeamCard";
 
 const meta: Meta<typeof TeamCard> = {
-    title: "Components/TeamCard",
-    component: TeamCard,
-    tags: ["autodocs"],
-    args: {
-        name: "Dream Team",
-        players: [
-            { name: "Lala", promoName: "MBA DFS", levelName: "Bronze" },
-            { name: "Toto", promoName: "MBA DFS", levelName: "Bronze" },
-        ],
-    },
+  title: "Components/TeamCard",
+  component: TeamCard,
+  tags: ["autodocs"],
+  args: {
+    name: "Dream Team",
+    players: [
+      {
+        name: "Lala",
+        firstname: "LalaLala",
+        promoName: "MBA DFS",
+        levelName: "Bronze"
+      },
+      {
+        name: "Toto",
+        firstname: "TotoToto",
+        promoName: "MBA DFS",
+        levelName: "Bronze"
+      }
+    ]
+  }
 };
 
 export default meta;
 type Story = StoryObj<typeof TeamCard>;
 
 export const Default: Story = {
-    args: {
-        name: "Dream Team",
-        players: [
-          { name: "Lala", promoName: "MBA DFS", levelName: "Bronze" },
-          { name: "Toto", promoName: "MBA DFS", levelName: "Bronze" },
-        ],
-    },
+  args: {
+    name: "Dream Team",
+    players: [
+      {
+        name: "Lala",
+        firstname: "LalaLala",
+        promoName: "MBA DFS",
+        levelName: "Bronze"
+      },
+      {
+        name: "Toto",
+        firstname: "TotoToto",
+        promoName: "MBA DFS",
+        levelName: "Bronze"
+      }
+    ]
+  }
 };
